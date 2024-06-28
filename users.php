19520b574a1032cb48477e14534d1906038a1408
MirkoValle <mirko.valle20@gmail.com>
2024-06-28 14:50:15 +0200

    Added scaffolding and users array

---
+++ users.php
@@ -0,0 +1,44 @@
+<?php
+
+$users = [
+    [
+        "username" => "alice_smith",
+        "password" => "Al1c3!Pass"
+    ],
+    [
+        "username" => "bob_johnson",
+        "password" => "B0b@1234"
+    ],
+    [
+        "username" => "carol_williams",
+        "password" => "C@rolP@ssw0rd"
+    ],
+    [
+        "username" => "david_jones",
+        "password" => "Dav1dP@ss"
+    ],
+    [
+        "username" => "eve_brown",
+        "password" => "Ev3_Secr3t"
+    ],
+    [
+        "username" => "frank_miller",
+        "password" => "Fr@nkP@ss"
+    ],
+    [
+        "username" => "grace_davis",
+        "password" => "Grac3!D@v1s"
+    ],
+    [
+        "username" => "hank_garcia",
+        "password" => "H@nkGarci@"
+    ],
+    [
+        "username" => "irene_martinez",
+        "password" => "Ir3neM@rt"
+    ],
+    [
+        "username" => "admin",
+        "password" => "admin"
+    ]
+];
